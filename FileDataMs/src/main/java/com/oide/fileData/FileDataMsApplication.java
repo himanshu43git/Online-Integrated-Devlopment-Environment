8fb428b73c4b1f3412a176b0e926a71a9541b82d
Himanshu Bhatt <himanshu43.work@gmail.com>
2025-06-22 05:17:04 +0530

    added the fileDataMS

---
+++ FileDataMs/src/main/java/com/oide/fileData/FileDataMsApplication.java
@@ -0,0 +1,13 @@
+package com.oide.fileData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileDataMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileDataMsApplication.class, args);
+	}
+
+}
---
+++ FileDataMs/src/test/java/com/oide/fileData/FileDataMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oide.fileData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileDataMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
